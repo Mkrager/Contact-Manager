546341632f700937a8609a53382d9f0d8454f1b9
Smaga Max <smaga.max@gmail.com>
2025-08-29 16:19:13 +0300

    add base entities

---
+++ ContactManager.Domain/Commons/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace ContactManager.Domain.Commons
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ ContactManager.Domain/Entites/Person.cs
@@ -0,0 +1,13 @@
+﻿using ContactManager.Domain.Commons;
+
+namespace ContactManager.Domain.Entites
+{
+    public class Person : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public DateTime DateOfBirth  { get; set; }
+        public bool Married { get; set; }
+        public string Phone { get; set; } = string.Empty;
+        public decimal Salary { get; set; }
+    }
+}
